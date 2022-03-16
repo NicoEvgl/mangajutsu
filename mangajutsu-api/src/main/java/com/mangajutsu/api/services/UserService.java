055ad97785756f22e7dfb72444b939779d6f457a
NicoEvgl <nico.evangelista33@gmail.com>
2022-03-16 19:42:53 +0100

    create user service

---
+++ mangajutsu-api/src/main/java/com/mangajutsu/api/services/UserService.java
@@ -0,0 +1,16 @@
+package com.mangajutsu.api.services;
+
+import com.mangajutsu.api.dao.entities.UserEntity;
+import com.mangajutsu.api.exceptions.UserAlreadyExistException;
+import com.mangajutsu.api.models.UserModel;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+    void register(UserModel user) throws UserAlreadyExistException;
+
+    boolean checkIfUserExist(String username);
+
+    UserEntity findByUsername(String username);
+}
---
+++ mangajutsu-api/src/main/java/com/mangajutsu/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package com.mangajutsu.api.services.impl;
+
+import com.mangajutsu.api.dao.entities.UserEntity;
+import com.mangajutsu.api.dao.repositories.UserRepository;
+import com.mangajutsu.api.exceptions.UserAlreadyExistException;
+import com.mangajutsu.api.models.UserModel;
+import com.mangajutsu.api.services.UserService;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public void register(UserModel user) throws UserAlreadyExistException {
+        // check if userEntity already exist
+        if (checkIfUserExist(user.getUsername())) {
+            throw new UserAlreadyExistException("Username already exist");
+        }
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+        encodePassword(userEntity, user);
+        userRepository.save(userEntity);
+    }
+
+    @Override
+    public boolean checkIfUserExist(String username) {
+        return userRepository.findByUsername(username) != null;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserEntity findByUsername(String username) {
+        UserEntity userEntity;
+        try {
+            userEntity = userRepository.findByUsername(username);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+        return userEntity;
+    }
+
+    private void encodePassword(UserEntity userEntity, UserModel user) {
+        userEntity.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
