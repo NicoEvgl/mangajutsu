f9d1f4525abd6b84befb0762479c2721e529f1c5
NicoEvgl <nico.evangelista33@gmail.com>
2022-03-16 19:59:41 +0100

    create user model

---
+++ mangajutsu-webclient/src/main/java/com/mangajutsu/webclient/models/UserModel.java
@@ -0,0 +1,113 @@
+package com.mangajutsu.webclient.models;
+
+import java.util.Collection;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserModel implements UserDetails {
+    private Integer userId;
+    @NotEmpty(message = "Le prénom doit être saisi")
+    private String firstName;
+    @NotEmpty(message = "Le nom doit être saisi")
+    private String lastName;
+    @NotEmpty(message = "Le pseudo doit être saisi")
+    private String username;
+    @NotEmpty(message = "L'email doit être saisi")
+    @Email(message = "Veuillez renseigner une adresse email valide")
+    private String email;
+    @NotEmpty(message = "Le mot de passe doit être saisi")
+    private String password;
+
+    // GETTERS AND SETTERS //
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // METHOD TO STRING //
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
