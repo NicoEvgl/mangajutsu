c774b04fceb4b06165c413a2f604241db4199531
NicoEvgl <nico.evangelista33@gmail.com>
2022-03-26 00:47:15 +0100

    prettier formating views

--- mangajutsu-webclient/src/main/java/com/mangajutsu/webclient/config/SecurityConfig.java
+++ mangajutsu-webclient/src/main/java/com/mangajutsu/webclient/config/SecurityConfig.java
@@ -36,8 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/login", "/register", "/register/verify")
                 .permitAll()
                 .anyRequest().authenticated()
